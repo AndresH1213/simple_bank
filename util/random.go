1a709ad91294c446a32f4a472379cf7a0823e53d
Andres Hernandez <edanhebla1213@hotmail.com>
2023-11-06 02:39:35 -0500

    helper functions for password, config and random generations

--- util/random.go
+++ util/random.go
@@ -43,7 +43,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"USD", "EUR", "CAD"}
+	currencies := []string{USD, EUR, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
@@ -52,4 +52,3 @@ func RandomCurrency() string {
 func RandomEmail() string {
 	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
-
